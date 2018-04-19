be2b361aee1f25489cff5e5c1c023f11c4323a1f
Mary Jack <maryjackinthebox@gmail.com>
2018-04-19 21:53:53 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,8 +6,8 @@ def key_for_min_value(name_hash)
   min_key = nil
   name_hash.each do |key, value|
     if min_value == nil
-      min_value == value
-      min_key== key
+      min_value = value
+      min_key= key
       elsif min_value >value
       min_value = value
       min_key = key
